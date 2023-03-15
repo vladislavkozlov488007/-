9ae569b58a5bf52f20c170f645480a7bbb19d638
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-03-15 10:08:13 +0300

    Add files via upload

---
+++ Finaltask.cs
@@ -0,0 +1,32 @@
+﻿// Итоговая задача. Напишите программу, которая из имеющегося массива строк форvирует массив из строк,
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо
+// задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше
+// обойтись исключительно массивами.
+
+string[] firstArray = {"Hello", "Second", "Rainbow", "No", "Egg"};
+string[] secondArray = new string [firstArray.Length];
+
+FindWords(firstArray, secondArray);
+System.Console.WriteLine($"Наш массив состоит из слов: ");
+PrintArray(firstArray);
+System.Console.WriteLine($"На выходе получаем: ");
+PrintArray(secondArray);
+
+void FindWords(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for(int i = 0; i < firstArray.Length; i++)
+        if(firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++) 
+        System.Console.Write($"{array[i]} ");
+    System.Console.WriteLine();
+}
+
